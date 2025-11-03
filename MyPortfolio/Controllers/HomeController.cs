d164a79375076be8eab436c667606a190ab00761
srinivasnikhil <srinivasnikhil38@gmail.com>
2025-11-03 13:01:09 -0500

    Bug Fixes and Mail Functionality Added

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Net.Mail;
 using Microsoft.AspNetCore.Mvc;
 using MyPortfolio.Models;
 
@@ -17,7 +18,34 @@ namespace MyPortfolio.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult SendMessage(string FullName, string Subject, string Email, string MobileNo, string Message)
+        {
+            try
+            {
+                using (MailMessage mail = new MailMessage())
+                {
+                    mail.From = new MailAddress("nikhilmallempatiadmissions@gmail.com");
+                    mail.To.Add("srinivasnikhil38@gmail.com");
+                    mail.Subject = "New Message from Contact Form";
+                    mail.Body = $"A Recruiter or Employer has sent you a message:\n\nName: {FullName}\nSubject: {Subject}\nEmail: {Email}\nMobile Number: {MobileNo}\nMessage: {Message}\n\nMr. Nikhil Respond to him immediately.";
+                    mail.IsBodyHtml = false;
 
+                    using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        smtp.Credentials = new System.Net.NetworkCredential("nikhilmallempatiadmissions@gmail.com", "bfyp xbac slcg vbqx");
+                        smtp.EnableSsl = true;
+                        smtp.Send(mail);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json("Error");
+            }
+
+            return Json("Success");
+        }
         public IActionResult Privacy()
         {
             return View();
